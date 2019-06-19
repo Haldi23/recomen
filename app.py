83b8cf07f32728f5e6f26b8f6019207cd702482f
logico_logica <>
2019-06-20 01:29:16 +0900

    がんばってる

--- app.py
+++ app.py
@@ -1,22 +1,13 @@
 from flask import Flask, render_template
-from models.scrape import booth_scraper
 
-app = Flask(__name__)
+app = Flask(__name__,
+            static_folder="frontend/dist",
+            template_folder="frontend/dist")
 
 
 @app.route('/')
 def index():
-    title = 'れこめん'
-    message = 'レモンだ？貴様この野郎'
-    return render_template('index.html', title=title, message=message)
-
-
-@app.route('/test')
-def test():
-    title = 'れこめん'
-    item_datas = booth_scraper.scrape('https://booth.pm/ja/items')
-    message = item_datas
-    return render_template('index.html', title=title, message=message)
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
